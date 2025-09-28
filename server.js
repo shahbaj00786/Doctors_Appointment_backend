1fcf2c74decc97943e540b23c281ee64d6986276
Shahbaj Hussain <shahbaj231@gmail.com>
2025-09-28 11:57:56 +0530

    url update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000;
 
 app.use(cors({
   origin: [
-    "http://localhost:5174", 
+    "https://doctors-appointment-frontend-alpha.vercel.app", 
   ],
   credentials: true
 }));
